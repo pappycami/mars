5b5a54c12811a23c7b26f86b9d14b6134a5e8a2b
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-03-24 13:34:04 +0300

    run-1

--- src/main/java/com/ainapapy/mars/controller/WelcomeController.java
+++ src/main/java/com/ainapapy/mars/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.ainapapy.mars.controllers;
+package com.ainapapy.mars.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloController {
+public class WelcomeController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, Spring Boot!";
