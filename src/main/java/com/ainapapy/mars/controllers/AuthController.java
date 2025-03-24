f4174e6952b1279d95e3666db274558dc62a77ac
TSIRY HERY Ny Aina Papy <nap@esokia-webagency.com>
2025-03-24 17:48:56 +0300

    add - security

---
+++ src/main/java/com/ainapapy/mars/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.ainapapy.mars.controllers;
+
+import com.ainapapy.mars.repositories.UtilisateurRepository;
+import com.ainapapy.mars.utils.JwtUtil;
+import com.ainapapy.mars.models.Utilisateur;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UtilisateurRepository utilisateurRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(UtilisateurRepository utilisateurRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.utilisateurRepository = utilisateurRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody Utilisateur utilisateur) {
+        utilisateur.setMotDePasse(passwordEncoder.encode(utilisateur.getMotDePasse()));
+        utilisateurRepository.save(utilisateur);
+        return "Utilisateur inscrit avec succès";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Utilisateur utilisateur) {
+        Utilisateur user = utilisateurRepository.findByEmail(utilisateur.getEmail())
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+        if (!passwordEncoder.matches(utilisateur.getMotDePasse(), user.getMotDePasse())) {
+            throw new RuntimeException("Mot de passe incorrect");
+        }
+        return jwtUtil.generateToken(user.getEmail());
+    }
+}
